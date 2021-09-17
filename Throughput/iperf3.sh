247612146a5042c12c1e7539f350f95e00e68be1
Selina Isak <seisak@edu.aau.at>
2021-09-17 10:57:51 +0200

    merged two files into one (iperf3_measure)

--- Throughput/iperf3.sh
+++ Throughput/iperf3.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
     echo "Invalid number of arguments!"
-    echo "Usage: $0 <Server-IP> <iterations>"
+    echo "Usage: $0 <Server-IP> <iterations> <connections>"
     exit
 fi
 SERVER_IP=$1
 ITERATIONS=$2
+CONNECTIONS=$3
 
 for ((i = 1; i <= $ITERATIONS; i++))
 do
     echo "------------------ ITERATION: $i -------------------"
     echo "------------------ DEFAULT MODE --------------------"
-    iperf3 -c $SERVER_IP -P 10
+    iperf3 -c $SERVER_IP -P $CONNECTIONS
 done
 
 for ((i = 1; i <= $ITERATIONS; i++))
 do
     echo "------------------ ITERATION: $i -------------------"
     echo "------------------ REVERSE MODE --------------------"
-    iperf3 -c $SERVER_IP -P 10 -R
+    iperf3 -c $SERVER_IP -P $CONNECTIONS -R
 done
 exit
